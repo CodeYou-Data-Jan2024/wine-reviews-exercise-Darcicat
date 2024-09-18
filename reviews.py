299e4cfd76054a0d155687df625647c70699003b
Darci <darcinevitt@gmail.com>
2024-09-18 19:46:34 -0400

    partial complete - creating the summary

--- reviews.py
+++ reviews.py
@@ -1,2 +1,13 @@
-# add your code here
+import pandas as pd
+
+wine_reviews = pd.read_csv('data/winemag-data-130k-v2.csv.zip')
+summary = (
+    wine_reviews.groupby('country')
+    .agg(
+        reviews=('title','count'), # counts the number of reviews
+        points=('points', 'mean') # calculate avg
+    )
+    .reset_index()
+)
+print(summary)
 
