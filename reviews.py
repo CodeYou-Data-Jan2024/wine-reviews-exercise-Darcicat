d9ddabbf7e16eeca5a3e4b739b411e5d8e8d7beb
Darci <darcinevitt@gmail.com>
2024-09-19 19:11:34 -0400

    success! saving summary csv to the data folder

--- reviews.py
+++ reviews.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 wine_reviews = pd.read_csv('data/winemag-data-130k-v2.csv.zip')
+#print(wine_reviews.columns)
+
 summary = (
     wine_reviews.groupby('country')
     .agg(
@@ -9,5 +11,10 @@ summary = (
     )
     .reset_index()
 )
+summary.sort_values(by=["reviews"], inplace=True, ascending=False)
+summary["points"] = summary["points"].round(1)
 print(summary)
+summary.to_csv("data/reviews-per-country.csv")
+#how to save this in the data folder?
+#summary.to_csv("reviews-per-country.csv", index=True)
 
