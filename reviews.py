ca02e2d5f82c726fdae92098bcc3ff7c186a2d32
Darci <darcinevitt@gmail.com>
2024-09-19 19:15:15 -0400

    final push? changing the title of a column and including index=True

--- reviews.py
+++ reviews.py
@@ -6,15 +6,14 @@ wine_reviews = pd.read_csv('data/winemag-data-130k-v2.csv.zip')
 summary = (
     wine_reviews.groupby('country')
     .agg(
-        reviews=('title','count'), # counts the number of reviews
+        count=('title','count'), # counts the number of reviews
         points=('points', 'mean') # calculate avg
     )
     .reset_index()
 )
-summary.sort_values(by=["reviews"], inplace=True, ascending=False)
+summary.sort_values(by=["count"], inplace=True, ascending=False)
 summary["points"] = summary["points"].round(1)
 print(summary)
-summary.to_csv("data/reviews-per-country.csv")
-#how to save this in the data folder?
-#summary.to_csv("reviews-per-country.csv", index=True)
+summary.to_csv("data/reviews-per-country.csv", index=True)
+
 
